3b067622076ba99e12536cfa84a6b4196d17d06c
Koushik(Kay) <55854583+brewmeakay@users.noreply.github.com>
2021-12-22 19:05:55 +0100

    coded reset button and added comments

--- script.js
+++ script.js
@@ -3,26 +3,26 @@ let rangeSlider = document.querySelector('#rangeSlider')
 let grid =document.querySelector('#grid')
 let currentSliderValue=16;  //default value
 createGrid(currentSliderValue);
-
 let colorPicker = document.querySelector('#color-picker');
+let resetButton = document.querySelector('#reset-icon');
 
-
+//looks for changes in rangeSlider
 rangeSlider.oninput = function ()
 {
-    currentSliderValue = this.value
-    sliderValue.innerHTML = `${currentSliderValue} x ${currentSliderValue}`;
-    console.log(currentSliderValue)
     clearGrid();
-    createGrid(currentSliderValue);
+    createGrid(this.value);
 };
 
+//clears the grid
 function clearGrid()
 {
     grid.innerHTML=""
 }
 
+//create the grid with given dimension
 function createGrid(dimension)
 {
+    sliderValue.innerHTML = `${dimension} x ${dimension}`
     for(i=0;i<dimension;i++)
     {
         let row_div = document.createElement('div');
@@ -36,10 +36,17 @@ function createGrid(dimension)
         grid.append(row_div)
     }
 }
-
+//sketch function
 grid.addEventListener('mouseover',function(e)
 {
     e.target.classList.add('change-color');
     colorPicker.oninput = function(){}
     e.target.style.backgroundColor = colorPicker.value;
+})
+
+//resetbutton
+resetButton.addEventListener('click',function()
+{
+    clearGrid();
+    createGrid(currentSliderValue);
 })
