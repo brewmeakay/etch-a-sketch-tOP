5dd1fb845a907703f5abd7d6ed5cb3f54c913b4b
Koushik(Kay) <55854583+brewmeakay@users.noreply.github.com>
2021-12-22 19:06:59 +0100

    cleaning up , added comments and formatted

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
 let sliderValue = document.querySelector('#slider-value')
 let rangeSlider = document.querySelector('#rangeSlider')
-let grid =document.querySelector('#grid')
-let currentSliderValue=16;  //default value
+let grid = document.querySelector('#grid')
+let currentSliderValue = 16;  //default value
 createGrid(currentSliderValue);
 let colorPicker = document.querySelector('#color-picker');
 let resetButton = document.querySelector('#reset-icon');
 
 //looks for changes in rangeSlider
-rangeSlider.oninput = function ()
-{
+rangeSlider.oninput = function () {
     clearGrid();
     createGrid(this.value);
 };
 
 //clears the grid
-function clearGrid()
-{
-    grid.innerHTML=""
+function clearGrid() {
+    grid.innerHTML = ""
 }
 
 //create the grid with given dimension
-function createGrid(dimension)
-{
+function createGrid(dimension) {
     sliderValue.innerHTML = `${dimension} x ${dimension}`
-    for(i=0;i<dimension;i++)
-    {
+    for (i = 0; i < dimension; i++) {
         let row_div = document.createElement('div');
         row_div.classList.add('row-div')
-        for(j=0;j<dimension;j++)
-        {
+        for (j = 0; j < dimension; j++) {
             let col_div = document.createElement('div')
             col_div.classList.add('col-div')
             row_div.append(col_div);
@@ -37,16 +32,14 @@ function createGrid(dimension)
     }
 }
 //sketch function
-grid.addEventListener('mouseover',function(e)
-{
+grid.addEventListener('mouseover', function (e) {
     e.target.classList.add('change-color');
-    colorPicker.oninput = function(){}
+    colorPicker.oninput = function () { }
     e.target.style.backgroundColor = colorPicker.value;
 })
 
 //resetbutton
-resetButton.addEventListener('click',function()
-{
+resetButton.addEventListener('click', function () {
     clearGrid();
     createGrid(currentSliderValue);
 })
